1a238fdc0beb8fb17db899ef89915c982ad69056
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2023-06-27 22:22:59 -0400

    Add project files.

---
+++ NameTransformer.UnitTests/TransformerTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+namespace NameTransformer.UnitTests;
+
+public class TransformerTests
+{
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void GivenAnInvalidName_WhenTransformIsCalled_ThenNullIsReturned(string name)
+    {
+        var result = Transformer.Transform(name);
+
+        result.Should().BeNull();
+    }
+
+    [Test]
+    public void GivenOnlyAFirstOrLastName_WhenTransformIsCalled_ThenTheStringIsReturnedUnchanged()
+    {
+        const string name = "Carlos";
+
+        var result = Transformer.Transform(name);
+
+        result.Should().Be(name);
+    }
+
+    [TestCase("Carlos Sainz", "Sarlos Cainz")]
+    [TestCase("Esteban Ocon", "Osteban Econ")]
+    [TestCase("Alex Albon", "Alex Albon")]
+    public void GivenAFullName_WhenTransformIsCalled_ThenTheNameIsTransformed(string fullName, string transformedName)
+    {
+        var result = Transformer.Transform(fullName);
+
+        result.Should().Be(transformedName);
+    }
+
+    [TestCase("Lance Stroll", "Strance Loll")]
+    [TestCase("Charles Leclerc", "Larles Checlerc")]
+    public void GivenAFullNameStartingWithSeveralConsonants_WhenTransformIsCalled_ThenTheNameIsTransformed(string fullName, string transformedName)
+    {
+        var result = Transformer.Transform(fullName);
+
+        result.Should().Be(transformedName);
+    }
+
+    [TestCase("Carlos  Sainz")]
+    [TestCase("Carlos Sainz  ")]
+    [TestCase("  Carlos Sainz")]
+    [TestCase("  Carlos Sainz  ")]
+    public void GivenAFullNameWithExtraWhitespace_WhenTransformIsCalled_ThenTheNameIsTransformed(string fullName)
+    {
+        var result = Transformer.Transform(fullName);
+
+        result.Should().Be("Sarlos Cainz");
+    }
+}
---
+++ NameTransformer/Transformer.cs
@@ -0,0 +1,62 @@
+﻿namespace NameTransformer;
+
+public static class Transformer
+{
+    private static readonly char[] _vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+    public static string? Transform(string fullName)
+    {
+        if (string.IsNullOrWhiteSpace(fullName))
+            return null;
+
+        if (!IsFullName(fullName))
+            return fullName;
+
+        var names = fullName.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+        return IsMultipleConsonantName(names) ? GetTransformedComplexName(names) :
+            GetTransformedSimpleName(names);
+    }
+
+    private static bool IsFullName(string fullName)
+    {
+        return fullName.Contains(' ');
+    }
+
+    private static int FindFirstVowel(string name)
+    {
+        return name.ToLower().IndexOfAny(_vowels);
+    }
+
+    private static bool IsMultipleConsonantName(string[] names)
+    {
+        var firstNameFirstVowel = FindFirstVowel(names[0]);
+        var lastNameFirstVowel = FindFirstVowel(names[1]);
+
+        return firstNameFirstVowel > 1 || lastNameFirstVowel > 1;
+    }
+
+    private static string GetTransformedSimpleName(string[] names)
+    {
+        var firstName = $"{GetFirstLetter(names[1])}{names[0][1..]}";
+        var lastName = $"{GetFirstLetter(names[0])}{names[1][1..]}";
+        
+        return $"{firstName} {lastName}";
+    }
+
+    private static string GetTransformedComplexName(string[] names)
+    {
+        var firstNameFirstVowel = FindFirstVowel(names[0]);
+        var lastNameFirstVowel = FindFirstVowel(names[1]);
+
+        var firstName =  $"{names[1][..lastNameFirstVowel]}{names[0][firstNameFirstVowel..]}";
+        var lastName = $"{names[0][..firstNameFirstVowel]}{names[1][lastNameFirstVowel..]}";
+
+        return $"{firstName} {lastName}";
+    }
+
+    private static char GetFirstLetter(string name)
+    {
+        return name[0];
+    }
+}
